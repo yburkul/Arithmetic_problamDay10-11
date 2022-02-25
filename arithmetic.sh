df93395026da00ecf9c89ac99f4b22482a306b8b
yburkul <yburkul123@gmail.com>
2022-02-25 15:06:55 +0530

    Added: decending order

--- arithmetic.sh
+++ arithmetic.sh
@@ -1,6 +1,7 @@
 #Sorting Arithmetic Computation Problem
 
 #Write program to take three inputs - a, b & c
+array=()
 
 read -p "Enter a number: " a
 read -p "Enter a number: " b
@@ -26,3 +27,20 @@ a[2]=${d[13]}
 a[3]=${d[14]}
 
 echo ${a[@]}
+
+#Show Result in descending order
+
+echo "Before sorting: " ${a[@]}
+       for(( i=0 ; i<=3 ; i++ ))
+       do
+           for (( j=i+1 ; j<=3 ; j++ ))
+           do
+              if (( ${a[i]} < ${a[j]} ))
+              then
+                   temp=${a[i]}
+                   a[$i]=${a[j]}
+                   a[$j]=$temp
+               fi
+           done
+       done
+echo "After sorting: " ${a[@]}
